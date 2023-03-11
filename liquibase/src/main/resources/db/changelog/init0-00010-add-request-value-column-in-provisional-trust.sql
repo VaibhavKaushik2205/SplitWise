57d7aeed93271de1cc42ec98dcdb39ada9eccd1e
vaibhav.kaushik <vaibhav.kaushik@navi.com>
2023-03-11 22:43:35 +0530

    remove needless files

--- liquibase/src/main/resources/db/changelog/init0-00010-add-request-value-column-in-provisional-trust.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset author:vaibhav-kaushik id:0011
-
-ALTER TABLE provisional_trust_data ADD COLUMN value jsonb;
-ALTER TABLE provisional_trust_data_history ADD COLUMN value jsonb;
--- liquibase/src/main/resources/db/changelog/init0-0011-add-index-on-pan.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset author:vaibhav-kaushik id:012
-
-CREATE INDEX IF NOT EXISTS customer_pan_idx ON customer (pan);
-
-CREATE INDEX IF NOT EXISTS customer_provisional_trust_data_idx ON provisional_trust_data (customer_reference_id);
-CREATE INDEX IF NOT EXISTS customer_provisional_trust_data_and_key_idx ON provisional_trust_data (customer_reference_id, key);
